c471af1fe86dc87acf7b5af36882bcd33dcc104c
fahrizaall <fahrizalm84@gmail.com>
2022-10-30 20:46:11 +0700

    fix database

--- application/config/database.php
+++ application/config/database.php
@@ -75,10 +75,10 @@ $query_builder = TRUE;
 
 $db['default'] = array(
 	'dsn'	=> '',
-	'hostname' => 'localhost',
-	'username' => 'root',
-	'password' => '',
-	'database' => 'task_management',
+	'hostname' => 'sql12.freesqldatabase.com',
+	'username' => 'sql12530685',
+	'password' => 'jZVYwrPWcF',
+	'database' => 'sql12530685',
 	'dbdriver' => 'mysqli',
 	'dbprefix' => '',
 	'pconnect' => FALSE,
